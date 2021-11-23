2290f64cec67f294d41cb56041e6d569bd6eb64a
Abhinav Sawarkar <abhisawarkar85@gmail.com>
2021-11-23 15:25:52 +0530

    Update readme

--- src/components/CalcScreen.js
+++ src/components/CalcScreen.js
@@ -5,7 +5,7 @@ const CalcScreen = () => {
     <div className="h-2/6 flex flex-col gap-y-3 justify-center items-end px-2">
       <div className="text-gray-500 font-semibold">45+23+100</div>
       <input
-        type="number"
+        type="text"
         className="bg-gray-50 text-gray-900 font-bold text-5xl w-full outline-none text-right"
         value={168}
         contentEditable="false"
--- src/components/UI/CalcKey.js
+++ src/components/UI/CalcKey.js
@@ -5,11 +5,13 @@ const CalcKey = (props) => {
   const keyClickHandler = () => {
     console.log(keyRef.current.value);
   };
+
+  const btnTypeClass = `btn-${props.category}`;
   return (
     <button
-      className={`text-3xl  w-full rounded-xl ${
+      className={`${btnTypeClass} text-3xl  w-full rounded-xl ${
         props.children === 0 ? "col-start-1 col-end-3" : ""
-      } btn-${props.category}  transition`}
+      }   transition`}
       type={props.type}
       onClick={keyClickHandler}
       value={props.value}
